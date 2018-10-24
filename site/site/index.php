acb1e8fcb7376f7f1ee5b63533436bec82cabc12
rorm158 <rorm158@hotmail.fr>
2018-10-24 14:33:14 +0200

    v1

--- site/site/index.php
+++ site/site/index.php
@@ -1,5 +1,23 @@
-<?php
-
-echo 'Hello Framework!3';
-echo 'Romain le con';
-?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Dynomonde</title>
+        <link rel="stylesheet" href="style.php" media="all" type="text/css" />
+          <link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet'>
+    </head>
+    <body>
+         <h1 class="principal">DYNOMONDE REMAKE</h1>
+        
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="jouer.php">Jouer</a></li>
+            <li><a href="commentjouer.php">Comment Jouer?</a></li>
+            <li><a href="forum.php">Forum</a></li>
+            <li><a href="wiki.php">Wiki</a></li>
+        </ul>
+        <div class="nbmaj">Mise à jour 0.0.1</div>
+        <img src="images\dino.png" align="left">
+        <div class="textemaj">Création de la base du site, avec le menu, les titres et quelques informations de base</div>
+    </body>
+</html>
