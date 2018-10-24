fe4c8dad6bdf9f56d9fc6650cf55873f32838f89
rorm158 <rorm158@hotmail.fr>
2018-10-24 14:19:22 +0200

    v2

---
+++ site/site/style.php
@@ -0,0 +1,85 @@
+<?php
+header( 'content-type: text/css' );
+?>
+html {
+    background-color: white;
+    background-image: -webkit-linear-gradient(270deg,rgb(230,233,233) 0%,rgb(216,221,221) 100%);
+    background-image: linear-gradient(270deg,rgb(230,233,233) 0%,rgb(216,221,221) 100%);
+    -webkit-font-smoothing: antialiased;
+
+}
+
+body {
+    margin:  auto;
+    padding: 2em 2em 4em;
+    max-width: 800px;
+    font-family: "Audiowide";
+    font-size: 16px;
+    color: #545454;
+    background-color: white;
+}
+
+h2 {
+    font-family:"Audiowide" ;
+    color: #232;
+    font-weight: bold;
+    line-height: 1.6;
+}
+
+h1{
+    color: #232;
+    text-align: center;
+    font-family:"Audiowide" ;
+}
+
+.principal{
+    font-size:50px;
+}
+
+p {
+    font-family: "Courier New", Courier, monospace;
+}
+
+ul {
+    list-style-type: none;
+    margin: 15px ;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333333;
+}
+
+li {
+    float: left;
+    font-family:"Audiowide" ;
+    }
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px;
+    text-decoration: none;
+
+}
+
+li a:hover {
+    background-color: #111111;
+}
+
+.nbmaj {
+ width:500px;
+ height:50px;
+ background:gray;
+ margin:10px;
+color: #ffffff;
+font-size: 40px;
+padding-left: 200px;
+}
+.textemaj {
+ width:400px;
+ height:300px;
+ background:gray;
+ margin-left:300px;
+ color: #ffffff;
+font-size: 20px;
+}
---
+++ site/site/wiki.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Dynomonde</title>
+        <link rel="stylesheet" href="style.php" media="all" type="text/css" />
+         <link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet'>
+    </head>
+    <body>
+         <h1 class="principal">WIKI</h1>
+        
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="jouer.php">Jouer</a></li>
+            <li><a href="commentjouer.php">Comment Jouer?</a></li>
+            <li><a href="forum.php">Forum</a></li>
+            <li><a href="wiki.php">Wiki</a></li>
+        </ul>
+        <p>
+            Bienvenue blabla blabla blabla blabla blabla blabla blabla blabla blablablabla Bienvenue blabla blabla blabla blabla blabla blabla blabla blabla blablablablaBienvenue blabla blabla blabla blabla blabla blabla blabla blabla blablablablaBienvenue blabla blabla blabla blabla blabla blabla blabla blabla blablablablaBienvenue blabla blabla blabla blabla blabla blabla blabla blabla blablablabla.    
+        </p>
+    </body>
+</html>
