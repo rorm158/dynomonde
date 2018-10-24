1d88a1d5286b431c60cc4347f4531996209c8f64
rorm158 <rorm158@hotmail.fr>
2018-10-24 14:49:29 +0200

    v1

--- site/site/index.php
+++ site/site/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
     <head>
-        <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <title>Dynomonde</title>
         <link rel="stylesheet" href="style.php" media="all" type="text/css" />
           <link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet'>
